2499e0e3d7a91683e8631af9dfc45ee317b31639
Alex Norris <a.norris97@icloud.com>
2020-09-24 18:48:43 -0500

    j

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
         response.json().then(function(data) {
         let satellite;
         let fetchedSatellites = data.satellites;
-        for (let i = 0; i++; i < fetchedSatellites.length) {
+        for (let i = 0;  i < fetchedSatellites.length; i++) {
           // tslint:disable-next-line: max-line-length
           satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(satellite);
